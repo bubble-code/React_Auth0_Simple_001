8fc950377c7300b9515e38e39b47fb926875cde3
= <=>
2020-09-02 18:26:55 +0200

    firts commit 2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { LoginButton } from './components/LoginButton';
 import { LogoutButton } from './components/LogoutButton';
 import { Profile } from './components/Profile';
 import { useAuth0 } from '@auth0/auth0-react'
+import CircleLoader from 'react-spinners/CircleLoader'
 
 function App() {
   const { isLoading } = useAuth0()
   return (
-    isLoading ? (<span>Loading...!</span>) : (<>
+    isLoading ? <CircleLoader /> : (<>
       <LoginButton />
       <LogoutButton />
       <Profile />
-    </>) 
+      </>)
   );
 }
 
