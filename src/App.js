8a2562a85505553fe5e7e6952bcfea43786d128c
= <=>
2020-09-02 17:03:07 +0200

    firts commint 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { LoginButton } from './components/LoginButton';
 import { LogoutButton } from './components/LogoutButton';
 import { Profile } from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { isLoading } = useAuth0()
   return (
-    <>
+    isLoading ? (<span>Loading...!</span>) : (<>
       <LoginButton />
       <LogoutButton />
       <Profile />
-    </>
+    </>) 
   );
 }
 
